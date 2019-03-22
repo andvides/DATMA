a1fb20d3853faff6a2b2cc198f5a9834d8a32e8e
andvides <32333067+andvides@users.noreply.github.com>
2019-03-22 11:32:11 -0500

    Create runDATMA.sh

---
+++ codes/runDATMA.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#COMPSs enviroment
+#export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/ #PATH to JAVA HOME
+#export PATH=$PATH:$JAVA_HOME/bin
+#export CLASSPATH=$JAVA_HOME/jre/lib/ext:$JAVA_HOME/lib/tools.jar
+
+config="$1" #configuration file
+mode="$2" #mode sequential (seq) or distributed (compss)
+
+if [ "$2" = 'seq' ]; then
+    echo "Running DATMA in sequential mode."
+    python datma_seq.py -f $config
+    python finalReport.py -f $config
+elif [ "$2" = 'compss' ]; then
+    echo "Running DATMA with COMPSs support."
+    runcompss --master_name=pc -d --lang=python ./datma.py -f $config
+    python finalReport.py -f $config
+else
+    echo "Select between sequential or  distributed mode (see DATMA manual)"
+    exit 1;
+fi
