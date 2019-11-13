01b29c0cb08afcc2a86e90043a4197fbd0defbc9
andvides <32333067+andvides@users.noreply.github.com>
2019-11-12 22:23:00 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,13 +8,6 @@
 cd tools
 mkdir bin
 
-#SDLS-lite 
-echo 'Installing FM-index library'
-git clone https://github.com/simongog/sdsl-lite.git
-cd sdsl-lite
-./install.sh
-cd ..
-
 #install selectFasta
 echo 'Installing selectFasta'
 cd selectFasta
@@ -29,31 +22,5 @@ make
 cp rapifilt ../bin/
 cd ..
 
-#install mergeNotCombined
-echo 'Installing mergeNotCombined'
-cd mergeNotCombined
-make
-cp mergeNotCombined ../bin/
-cd ..
 
-#install mapping (SDSL installed)
-echo 'Installing mapping'
-cd mapping
-make
-cp mapping ../bin/
-cd ..
-
-#genFm9
-echo 'Installing genFM9'
-cd genFm9
-make
-cp genFm9 ../bin/
-cd ..
-
-#genFm9
-echo 'Installing binning'
-cd binning
-make
-cp binning ../bin/
-cd ..
 
