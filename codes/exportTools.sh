2d3501cdf7d03b781be9587eaca96867cbed2d0b
andvides <32333067+andvides@users.noreply.github.com>
2020-05-12 13:39:35 -0500

    Delete exportTools.sh

--- codes/exportTools.sh
+++
@@ -1,5 +0,0 @@
-export PATH=~/tools/DATMA/tools/mapping/:$PATH
-export PATH=~/tools/DATMA/tools/binning/:$PATH
-export PATH=~/tools/DATMA/tools/genFm9/:$PATH
-export PATH=/home/software/src/DATMA/tools/kaiju/bin/:$PATH
-
